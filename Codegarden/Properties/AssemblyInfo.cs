7e108b83a8164b413a7d2a37bb6cb33636035ad5
Simon <schandorff1@hotmail.com>
2017-11-22 00:56:28 +0100

    Changed entire package
    Hopefully works now

--- Codegarden/Properties/AssemblyInfo.cs
+++ Codegarden/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Codegarden")]
+[assembly: AssemblyTitle("CodeGarden")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Codegarden")]
+[assembly: AssemblyProduct("CodeGarden")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ae4b1e74-0388-4985-b6df-ef40cc3fedd8")]
+[assembly: Guid("d4afa99d-04bc-405a-84c1-34c1b1b44890")]
 
 // Version information for an assembly consists of the following four values:
 //
